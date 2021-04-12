609118b4f4a14bc01991aca3cd689abb88e5abb6
Akim Kingori <akim.king12@gmail.com>
2021-04-12 18:08:08 +0300

    Added transaction addition

--- src/components/Balance.js
+++ src/components/Balance.js
@@ -1,12 +1,11 @@
 import React, { useContext } from 'react'
-import Transaction from './Transaction'
 import { GlobalContext } from '../context/GlobalState'
 
 const Balance = () => {
 
     const { transactions } = useContext(GlobalContext)
     const amounts = transactions.map(transaction => transaction.amount);
-
+    console.log(amounts);
     const total = amounts.reduce((acc, item) => (acc += item), 0).toFixed(2);
     return (
         <div>

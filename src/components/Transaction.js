0566eb0f3e10e0516b91004297ec4a27b54f4fd8
Akim Kingori <akim.king12@gmail.com>
2021-04-12 15:41:00 +0300

    Added reducer and provider

---
+++ src/components/Transaction.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Transaction = ({ transaction }) => {
+
+    const sign = transaction.amount < 0 ? '-' : '+';
+
+
+    return (
+        <div>
+            <li className={transaction.amount < 0 ? 'minus' : 'plus'} key={transaction.id}>
+                {transaction.text} <span>{sign}${Math.abs(transaction.amount)}</span><button className="delete-btn">x</button>
+            </li>
+        </div>
+    )
+}
+
+
+export default Transaction
